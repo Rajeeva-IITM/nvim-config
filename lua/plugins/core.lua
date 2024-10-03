9a2e9800643057e997f7e61ab43865743216ffb5
Rajeeva-IITM <bt20d204@smail.iitm.ac.in>
2024-10-03 10:46:21 +0530

    First time lazy Neovim configuration

---
+++ lua/plugins/core.lua
@@ -0,0 +1,15 @@
+return {
+  { "LazyVim/LazyVim", opts = {
+    colorscheme = "catppuccin",
+  } },
+  {
+    "folke/twilight.nvim",
+    opts = {
+      {
+        dimming = {
+          alpha = 0.25,
+        },
+      },
+    },
+  },
+}
